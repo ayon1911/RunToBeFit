72087b540381eb008c37e07b93ce6bee9df99308
ayon <kr_ayon@icloud.com>
2018-12-25 12:53:39 +0100

    Initial Commit

---
+++ RunToBeFit/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RunToBeFit
+//
+//  Created by krAyon on 25.12.18.
+//  Copyright © 2018 DocDevs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RunToBeFit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RunToBeFit
+//
+//  Created by krAyon on 25.12.18.
+//  Copyright © 2018 DocDevs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
